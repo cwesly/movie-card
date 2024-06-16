2efd14b7b798437b90fc898de7e016b9e91632ce
josuejcs13 <josuejcs13@gmail.com>
2024-06-15 23:53:36 -0400

    feat: redirect for home and add logic focus

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,17 +1,28 @@
 import { useEffect, useState } from "react";
 import Input from "../../components/input";
-import { InputType, TypeError } from "../../types";
+import { InputType, TypeError, TypeIsTouched } from "../../types";
 import Button from "../../components/button";
 import IconNetflix from "../../components/iconNetflex";
 import ErrorForm from "../../components/errorForm";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [inputEmail, setInputEmail] = useState<string>("");
   const [inputPassword, setInputPassword] = useState<string>("");
+  const navigate = useNavigate();
   const [error, setError] = useState<TypeError>({
     email: "Please enter a valid email or phone number.",
     password: "Your password must contain between 4 and 60 characters.",
   });
+  const [isTouched, setIsTouched] = useState<TypeIsTouched>({
+    email: false,
+    password: false,
+  });
+
+  const user = {
+    email: "j@r.com",
+    password: "1234",
+  };
 
   const validatePassword = () => {
     if (inputPassword.length >= 4 && inputPassword.length <= 60) {
@@ -28,7 +39,7 @@ const Login = () => {
   };
 
   const validateEmail = () => {
-    const regex = /^[\w\-.]+@([\w-]+\.)+[\w-]{2,}$/;
+    const regex = /^[\w\-.]+@([\w-]+\.)+[\w-]{2,5}$/;
     if (regex.test(inputEmail)) {
       setError({
         ...error,
@@ -52,7 +63,22 @@ const Login = () => {
 
   const isValid = Boolean(error.email) || Boolean(error.password);
 
-  console.log(isValid);
+  const handleSubmit = () => {
+    if (user.email === inputEmail && user.password === inputPassword) {
+      navigate("/home");
+      return;
+    }
+    alert("errado");
+  };
+
+  const handleBlur = () => {
+    setIsTouched({
+      ...isTouched,
+      email: true,
+      password: true,
+    });
+    validateEmail();
+  };
 
   return (
     <div className="@apply flex justify-center bg-black/50 h-screen items-center relative">
@@ -61,27 +87,53 @@ const Login = () => {
         src="https://assets.nflxext.com/ffe/siteui/vlv3/cacfadb7-c017-4318-85e4-7f46da1cae88/f2e98033-57b5-4cf9-993d-ee774904fc5e/CA-en-20240603-popsignuptwoweeks-perspective_alpha_website_small.jpg"
       />
       <IconNetflix size="h-10" className="absolute top-7 left-8 " />
-      <form className="@apply flex flex-col justify-center bg-black/75 px-16 pb-32 rounded-3.5 pt-14 gap-4 w-[28.125rem] text-white">
+      <form className="@apply flex flex-col justify-center bg-black/75 px-16 pb-24 rounded-3.5 pt-11 gap-4 w-[28.125rem] text-white">
         <h1 className="@apply text-[white] font-semibold text-[1.9375rem] pb-2">
           Sing In
         </h1>
         <Input
+          className={isTouched.email && error.email ? "border-red-600" : ""}
           setValue={setInputEmail}
           value={inputEmail}
           typeInput={InputType.Email}
           placeholder="Email or phone number"
+          onFocus={() =>
+            setIsTouched({
+              ...isTouched,
+              email: false,
+            })
+          }
+          onBlur={handleBlur}
         />
-        {error.email && <ErrorForm>{error.email}</ErrorForm>}
+        {isTouched.email && error.email ? (
+          <ErrorForm>{error.email}</ErrorForm>
+        ) : (
+          ""
+        )}
         <Input
+          className={
+            isTouched.password && error.password ? "border-red-600" : ""
+          }
           setValue={setInputPassword}
           value={inputPassword}
           typeInput={InputType.Password}
           placeholder="Password"
+          onFocus={() =>
+            setIsTouched({
+              ...isTouched,
+              password: false,
+            })
+          }
+          onBlur={handleBlur}
         />
-        {error.password && <ErrorForm>{error.password}</ErrorForm>}
+        {isTouched.password && error.password ? (
+          <ErrorForm>{error.password}</ErrorForm>
+        ) : (
+          ""
+        )}
         <Button
-          color="bg-[#e50914] disabled:bg-gray-600 disabled:cursor-not-allowed"
-          handleSubmit={() => alert("oi")}
+          color="bg-[#e50914] disabled:cursor-not-allowed"
+          handleSubmit={handleSubmit}
           isDisable={isValid}
         >
           Sing In
